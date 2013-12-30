6459b488c1d98123577f4feae1317906f4ee4b0a
olena.kutsenko <olena.kutsenko@gmail.com>
2013-12-31 00:51:33 +0100

    changed port to use the one assigned by heroku

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ var server = http.createServer(function (request, response) {
     serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function () {
+server.listen(process.env.PORT || 3000, function () {
     console.log("Server listening on port 3000.");
 });
 
